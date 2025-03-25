adca739a526c72cfd708ee94bb60c2b54f916544
IgorShevtshenko <89193373+IgorShevtshenko@users.noreply.github.com>
2025-03-25 20:38:50 +0200

    Add API key to on demand resources (#2)

---
+++ Tuist/ProjectDescriptionHelpers/ProjectDescription+Settings.swift
@@ -0,0 +1,10 @@
+import ProjectDescription
+
+public extension Settings {
+    
+    static let settings: Settings = .settings(
+        base: [
+            "SWIFT_VERSION": "6.0"
+        ]
+    )
+}
