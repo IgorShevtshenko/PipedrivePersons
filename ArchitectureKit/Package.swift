58695a03cb46d8c3c86cd3297a13fbd1c5963979
IgorShevtshenko <89193373+IgorShevtshenko@users.noreply.github.com>
2025-03-28 23:26:11 +0200

    Add localization (#7)

--- ArchitectureKit/Package.swift
+++ ArchitectureKit/Package.swift
@@ -10,16 +10,25 @@ let package = Package(
             name: "ArchitectureKit",
             targets: ["ArchitectureKit"]
         ),
+        .library(
+            name: "ArchitectureTesting",
+            targets: ["ArchitectureTesting"]
+        )
     ],
     dependencies: [
         .package(path: "./Utils")
     ],
     targets: [
         .target(name: "ArchitectureKit"),
+        .target(
+            name: "ArchitectureTesting",
+            dependencies: ["Utils"]
+        ),
         .testTarget(
             name: "ArchitectureKitTests",
             dependencies: [
                 "ArchitectureKit",
+                "ArchitectureTesting",
                 "Utils",
             ]
         ),
